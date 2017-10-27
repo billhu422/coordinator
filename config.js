d936044168b44e8c4bf1c686167340831a4799b9
xiaowei hu <billhu422@126.com>
2017-10-27 14:37:55 +0800

    联调成功

--- config.js
+++ config.js
@@ -1,4 +1,22 @@
 var config = {}
 config.db = {
-    connection:123
+    host:'124.251.62.215',
+    port:'3306',
+    user: 'root',
+    password: 'my-secret-pw',
 }
+
+config.oauth = {
+    account_server: 'http://124.251.62.217:8000',
+    client_id: '9c9bca6d26234ff98fb93df3e3d065c5',
+    client_secret: 'e1396a545ace426a941c0edfd3403d0f',
+    grant_type : 'password',
+    username : 'laoguo.cn@gmail.com',
+    password: '12345',
+};
+
+config.delivery = {
+    service_endpoint:'http://124.251.62.216:3002/v1/hybrid/qcloud/cvm',
+}
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('koa-router')()
-var mysql = require('mysql');
+const mysql = require('mysql')
+const request = require('request')
+var config = require("../config");
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!'
@@ -16,43 +18,106 @@ router.get('/json', async (ctx, next) => {
   }
 })
 
-router.post('/delivery',async(ctx,next) =>{
-    ctx.body = 'I am delivering :)'
+asyncQuery= async (client)=>{
+    let bd = await new Promise(function(resolve, reject) {
+        client.query('select PRODUCT_ORDER.ID,PRODUCT_ORDER.STATE_,NOTE.TEXT from PRODUCT_ORDER,NOTE WHERE PRODUCT_ORDER.ID = NOTE.NOTE_PRODUCT_ORDER_ID and PRODUCT_ORDER.STATE_=\'InProgress\' and NOTE.TEXT =  \'unpaid\'',
+        function(err,  results, fields) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+    return bd;
+}
+
+asyncUpdate= async (client,order_id,note_text)=>{
+    let bd = await new Promise(function(resolve, reject) {
+        client.query('update NOTE set TEXT=' + '\''+note_text+'\'' + 'where NOTE.NOTE_PRODUCT_ORDER_ID=' + order_id +';',
+        function(err,  results, fields) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+    return bd;
+}
+
+asyncRequest= async (opts)=>{
+    let bd = await new Promise(function(resolve, reject) {
+        request(opts, function(err,r, data) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+    return bd;
+}
+
+router.post('/whip',async(ctx,next) =>{
+    ctx.body = 'I am whipping :)'
     //1.read from mysql
     //2.update note to paying
     //3.update note to paied or ''
     //4.exit
 
-
-  var TEST_DATABASE = 'DSPRODUCTORDERING';
-  var TEST_TABLE = 'PRODUCT_ORDER';
-  var client = mysql.createConnection({
-      host:'124.251.62.215',
-      port:'3306',
-    user: 'root',
-    password: 'my-secret-pw',
-
-  });
-  client.connect();
-  client.query("use " + TEST_DATABASE);
-  client.query(
-  'select PRODUCT_ORDER.ID,PRODUCT_ORDER.STATE_,NOTE.TEXT from PRODUCT_ORDER,NOTE WHERE PRODUCT_ORDER.ID = NOTE.NOTE_PRODUCT_ORDER_ID;',
-  function selectCb(err, results, fields) {
-    if (err) {
-      throw err;
+    var TEST_DATABASE = 'DSPRODUCTORDERING';
+    var TEST_TABLE = 'PRODUCT_ORDER';
+    var client = mysql.createConnection({
+        host:config.db.host,
+        port:config.db.port,
+        user:config.db.user,
+        password: config.db.password,
+    });
+    client.connect();
+    client.query("use " + TEST_DATABASE);
+    var data = await asyncQuery(client)
+    //read order & update note.text to Paid
+    if(data.length == 0){
+        console.log('nothing');
+        return;
+    }else{
+        for(var i = 0; i < data.length; i++) {
+            //console.log("%s\t%s\t%s",data[i].ID,data[i].STATE_,data[i].TEXT)
+            var x = await asyncUpdate(client,data[i].ID,'Paid');
+        }
     }
-       if(results)
-      {
-          for(var i = 0; i < results.length; i++)
-          {
-              if(results[i].STATE_ == 'Acknowledged' && results[i].TEXT != 'Paid') {
-                  console.log("%s\t%s\t%s", results[i].ID, results[i].STATE_, results[i].TEXT);
-              }
-          }
-      }
     client.end();
-  }
-);
+
+    //fetch adminAccessToken
+    var key = config.oauth.client_id + ':' + config.oauth.client_secret;
+    var base64 = (new Buffer(key)).toString('base64');
+
+    var opts_fechadminAccessToken = {
+         method: "POST",
+         url: config.oauth.account_server + '/oauth2/token',
+         headers: {
+            'Authorization' : 'Basic ' + base64,
+            'Content-Type' : 'application/x-www-form-urlencoded'
+         },
+         form: {'grant_type':config.oauth.grant_type,'username':config.oauth.username,'password':config.oauth.password}
+    }
+    var tokenData = await asyncRequest(opts_fechadminAccessToken)
+    var adminAccessToken = JSON.parse(tokenData).access_token
+    //console.log(adminAccessToken)
+
+    //delivery cvm
+    for (var j = 0; j < data.length; j++){
+        var opts_deliverycvm = {
+            method: "POST",
+            headers: {'content-type' : 'application/x-www-form-urlencoded','Authorization':'Bearer '+ adminAccessToken},
+            url:     config.delivery.service_endpoint,
+            form:    {'orderId':data[j].ID}
+        }
+        //console.log(opts_deliverycvm)
+        cvmdata = await asyncRequest(opts_deliverycvm)
+        console.log(cvmdata)
+    }
 })
 
 module.exports = router
